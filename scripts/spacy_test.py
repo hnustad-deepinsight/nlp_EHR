df4a554878f3ea9c1086c321f3a3a93456b6fce5
hnustad-deepinsight <hnustad@deepinsight.io>
2022-02-01 12:34:08 +0100

    readme updated with norwegian bokmål model loading

--- scripts/spacy_test.py
+++ scripts/spacy_test.py
@@ -1,4 +1,13 @@
-import spacy as sp
+import spacy
+from spacy.lang.nb.examples import sentences
+
+nlp = spacy.load("nb_core_news_md")
+doc = nlp(sentences[0] + ' ' + sentences[1] + ' ' + sentences[2] + ' ' + sentences[3])
+print(doc.text)
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
+
+
 
 
 
